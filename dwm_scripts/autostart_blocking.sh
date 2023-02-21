d7d8062e4f6c7ad8a53618fb52ab1665c15c4e9f
Mateusz Bagieński <mateuszb6a@gmail.com>
2023-02-21 21:03:26 +0100

    Add dwm autostart scripts

---
+++ dwm_scripts/autostart_blocking.sh
@@ -0,0 +1,4 @@
+xrandr --output HDMI-1 --mode 1920x1080 --rate 144.00 --primary;
+xrandr --output eDP-1 --mode 1920x1200 --rate 60.00 --right-of HDMI-1;
+setxkbmap pl;
+wal -R;
--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -7,6 +7,7 @@ ln -s $HOME/dotfiles/config/x11 ~/.config
 ln -s $HOME/dotfiles/.zshenv ~/.zshenv
 ln -s $HOME/dotfiles/config/nvim ~/.config
 ln -s $HOME/dotfiles/config/dunst ~/.config
+ln -s $HOME/dotfiles/dwm_scripts ~/.dwm
 # ln -s $HOME/dotfiles/mpd ~/.config
 # ln -s $HOME/dotfiles/ncmpcpp ~/.config
 # ln -s $HOME/dotfiles/mpv ~/.config
